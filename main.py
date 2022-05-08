6129063f8a2f472c2d04c73de4d7af865153bb30
neel-jotaniya <jotaniyaneel07@gmail.com>
2022-05-08 13:37:28 +0530

    API key change on failure

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from flask import request,render_template,Flask, url_for
 app = Flask(__name__)
 
 
+api_key_list = ["90cdd48823dd4508943512eca03110ec","e9980fbb7ab142e092b142fdcab23862"]
+for api in api_key_list:
+          
+          api_link = f"https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey={api}"
 
-
+          r = requests.get(api_link)
+          if r.status_code == 200:
+               break
 
 def data_modifier(article_list):
      for x in article_list:
@@ -17,6 +23,8 @@ def data_modifier(article_list):
                x['urlToImage'] = 'https://res.cloudinary.com/dzf5noyqz/image/upload/v1651941941/NewsBite/noImage_tiznad.png'
           if x['description'] == None:
                article_list.remove(x)
+          # if len(x['title']) <= 97 :
+          #      x['title'] = x['title'] + (97 - len(x['title']))*" "
      return article_list
 
 def date_changer(date_str):
@@ -34,32 +42,39 @@ def home():
      if request.method == "POST":
           name = request.form["search"]
           
-          api_link = f"https://newsapi.org/v2/everything?q={name}&language=en&pageSize=100&apiKey=e9980fbb7ab142e092b142fdcab23862"
+          api_link = f"https://newsapi.org/v2/everything?q={name}&language=en&pageSize=100&apiKey={api}"
           r = requests.get(api_link)
+          
 
           data = json.loads(r.content)
-
+          
           main_data = data["articles"]
           all_news = data_modifier(main_data)
-          
+
           return render_template("index.html",all_news = all_news)
-     api_link = "https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=e9980fbb7ab142e092b142fdcab23862"
+         
+          
+     for api in api_key_list:
+          
+          api_link = f"https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey={api}"
 
-     r = requests.get(api_link)
-
+          r = requests.get(api_link)
+          if r.status_code == 200:
+               break
      data = json.loads(r.content)
-     main_data = data["articles"]
-     all_news = data_modifier(main_data)
-     
-     return render_template("index.html",all_news = all_news)
-
+     if data['status'] != 'error':
+          main_data = data["articles"]
+          all_news = data_modifier(main_data)
+          return render_template("index.html",all_news = all_news)
+     else:
+          return render_template('error.html')
 @app.route("/top_news/<string:name>")                 
 def headline(name): 
      
           if name == "top-headlines":
-               api_link = "https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=e9980fbb7ab142e092b142fdcab23862"
+               api_link = f"https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey={api}"
           else:          
-               api_link = f"https://newsapi.org/v2/top-headlines?country=in&category={name}&pageSize=100&apiKey=e9980fbb7ab142e092b142fdcab23862"
+               api_link = f"https://newsapi.org/v2/top-headlines?country=in&category={name}&pageSize=100&apiKey={api}"
           r = requests.get(api_link)
      
           data = json.loads(r.content)
@@ -68,6 +83,3 @@ def headline(name):
           all_news = data_modifier(main_data)
          
           return render_template("index.html",all_news = all_news)
-
-
-     
